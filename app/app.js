0c41d2643e23272c572df951b78c2d49deb880e3
Kyle Warneck <kyle.warneck@tubemogul.com>
2015-10-16 17:01:54 -0700

    added routing

--- app/app.js
+++ app/app.js
@@ -1,42 +1,50 @@
-var myApp = angular.module('laundryApp',['firebase'])
-myApp.controller('BuildingsController', ['$scope','$firebaseArray',function($scope, $firebaseArray){
-    $scope.buildings = []
-}])    
-myApp.controller('MachinesController', ['$scope','$firebaseArray', function($scope, $firebaseArray) {
-    var firebase = new Firebase("https://blinding-heat-2671.firebaseio.com/");
-    // $scope.messages = $firebaseArray(ref);
-    this.machines = [{name: 'washer'}];
+var myApp = angular.module('laundryApp',['firebase','ngRoute'])
 
+// var firebase = new Firebase("https://blinding-heat-2671.firebaseio.com/");
 
-// var todoList = this;
-// todoList.todos = [
-//   {text:'learn angular', done:true},
-//   {text:'build an angular app', done:false}];
+// phonecatApp.config(['$routeProvider',
+//   function($routeProvider)
+myApp.config(['$routeProvider', function($routeProvider) {
 
-// todoList.addTodo = function() {
-//   todoList.todos.push({text:todoList.todoText, done:false});
-//   todoList.todoText = '';
-// };
+    $routeProvider
 
-// todoList.remaining = function() {
-//   var count = 0;
-//   angular.forEach(todoList.todos, function(todo) {
-//     count += todo.done ? 0 : 1;
-//   });
-//   return count;
-// };
+    .when('/', {
+        templateUrl: 'components/buildings/buildings.html',
+        // controller: 'BuildingsController'
+    })
 
-// todoList.archive = function() {
-//   var oldTodos = todoList.todos;
-//   todoList.todos = [];
-//   angular.forEach(oldTodos, function(todo) {
-//     if (!todo.done) todoList.todos.push(todo);
-//   });
-// };
+    .when('/:buildingId', {
+        templateUrl: 'components/machines/machines.html'
+        // controller: 'MachinesController'
+    })
+
+    .when('/:buildingId/:machineId', {
+        templateUrl: 'components/machines/machine.html'
+        // controller: 'MachinesController'
+    })
+
+    .otherwise({
+        redirectTo: '/buildings'
+    });
+    // $locationProvider.html5Mode(true);
+
 }]);
 
+myApp.controller('BuildingsController', ['$scope','$firebaseArray',function($scope, $firebaseArray){
+    $scope.buildings = ['i am a bulding'];
+}])    
 
-// angular.module('laundryApp',[])
-// .controller('MachinesListController', ['', function(){
+myApp.controller('MachinesController', ['$scope','$firebaseArray', function($scope, $firebaseArray) {
+    
+    // $scope.messages = $firebaseArray(ref);
+    $scope.machines = [{name: 'washer'}];
 
-// }])
+    // todoList.archive = function() {
+    //   var oldTodos = todoList.todos;
+    //   todoList.todos = [];
+    //   angular.forEach(oldTodos, function(todo) {
+    //     if (!todo.done) todoList.todos.push(todo);
+    //   });
+    // };
+}]);
+
